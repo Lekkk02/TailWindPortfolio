e0cccef009a14b8e3653ee087776fecd1c8ce572
Lekkk02 <maikel.g972@gmail.com>
2023-05-01 16:48:23 -0400

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      ml: "898px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 95%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "rgb(48, 48, 48);",
+        bestWhite: "rgb(215 215 215 / 88%);",
+      },
+      maxWidth: {
+        250: "250px",
+      },
+    },
+  },
+  plugins: [],
+};
